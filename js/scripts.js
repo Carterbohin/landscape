de0ce0c5b1fe3ec20b2df03c24492b72285d8db4
Carter Bohin <carterbohin@gmail.com>
2024-11-19 10:54:07 -0700

    added some images

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@ btnElement.addEventListener('click', () => {
     btnElement.classList.toggle('open')
     navElement.classList.toggle('open')
 });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'cube',
+    speed: 2000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+//start auto play
+    autoplay: {
+        delay: 5000,
+    },
+})
+  
+
